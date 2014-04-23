09dc887a475db97411449d6b5cf9f27e407f2ce0
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-04-23 19:15:25 +0200

    Add vector typedefs.

---
+++ vpp/vector.hh
@@ -0,0 +1,35 @@
+#ifndef VPP_VECTOR_HH__
+# define VPP_VECTOR_HH__
+
+# include <Eigen/Core>
+
+namespace vpp
+{
+
+  template <typename T, unsigned N>
+  using vector = Eigen::Matrix<T, N, 1>;
+
+#define VPP_ALIAS_DECL(T1, T2)                  \
+  template <unsigned N>                         \
+  using v##T2 = vector<T1, N>;               \
+  typedef v##T2<1> v##T2##1;                    \
+  typedef v##T2<2> v##T2##2;                    \
+  typedef v##T2<3> v##T2##3;                    \
+  typedef v##T2<4> v##T2##4;
+
+  VPP_ALIAS_DECL(char, char);
+  VPP_ALIAS_DECL(short, short);
+  VPP_ALIAS_DECL(int, int);
+  VPP_ALIAS_DECL(float, float);
+  VPP_ALIAS_DECL(double, double);
+
+  VPP_ALIAS_DECL(unsigned char, uchar);
+  VPP_ALIAS_DECL(unsigned short, ushort);
+  VPP_ALIAS_DECL(unsigned int, uint);
+
+#undef VPP_ALIAS_DECL
+
+
+};
+
+#endif
