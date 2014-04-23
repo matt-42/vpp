90c9a892867664f825489d0e4ada95fbb2e515b9
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-04-23 19:14:43 +0200

    Add imageNd test.

---
+++ tests/imageNd.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vpp/imageNd.hh>
+
+int main()
+{
+  using vpp::imageNd;
+  using vpp::vint2;
+
+  int dims[] = {100, 200};
+  vpp::imageNd<int, 2> img(dims);
+
+  assert(img.domain().size(0) == dims[0]);
+  assert(img.domain().size(1) == dims[1]);
+
+  assert(img.data() == img.begin());
+
+  assert(img.coords_to_index(vpp::vint2::Ones()) == dims[1] + 1);
+
+  for (int r = 0; r < img.domain().size(0); r++)
+    for (int c = 0; c < img.domain().size(1); c++)
+    {
+      assert(img.coords_to_index(vint2(r, c)) == (r * dims[1] + c));
+    }
+
+  for (int r = 0; r < img.domain().size(0); r++)
+    for (int c = 0; c < img.domain().size(1); c++)
+    {
+      img(vpp::vint2(r, c)) = r * c;
+    }
+
+
+  for (int r = 0; r < img.domain().size(0) - 1; r++)
+    for (int c = 0; c < img.domain().size(1) - 1; c++)
+    {
+      assert(img(vpp::vint2(r, c)) == r * c);
+    }
+
+}
