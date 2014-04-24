5fbcc1bd2d67f9350ed7e97fdc934ed05ab5bb34
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-04-24 19:35:00 +0200

    Add vpp.hpp

---
+++ vpp/vpp.hh
@@ -0,0 +1,14 @@
+#ifndef VPP_VPP_HH_
+# define VPP_VPP_HH_
+
+
+# include <vpp/imageNd.hh>
+# include <vpp/image2d.hh>
+# include <vpp/image3d.hh>
+
+# include <vpp/vector.hh>
+# include <vpp/neighborhood.hh>
+# include <vpp/boxNd.hh>
+# include <vpp/parallel_for.hh>
+
+#endif
