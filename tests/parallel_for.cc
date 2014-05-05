88e5197ff55e5459e4b22d25d7e3be5b3da7e526
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-05-05 02:33:17 +0000

    More tests for parallel_for.

--- tests/parallel_for.cc
+++ tests/parallel_for.cc
@@ -7,8 +7,9 @@ int main()
   using vpp::parallel_for_openmp;
   using vpp::image2d;
   using vpp::vint2;
+  using vpp::box2d;
 
-  image2d<int> img2(500, 300);
+  image2d<int> img2(50, 300);
 
   parallel_for_openmp(img2.domain()) << [&] (vint2 p)
   {
@@ -16,8 +17,33 @@ int main()
   };
 
   for (auto p : img2.domain())
-  {
     assert(img2(p) == 42);
+
+  // One images
+  vpp::pixel_wise(img2) << [&] (auto& p) { p = 43; };
+
+  for (auto p : img2.domain()) { assert(img2(p) == 43); }
+
+  vpp::row_wise(img2) << [&] (auto& p) { p = 44; };
+  for (auto p : img2.domain()) {
+    if (p[1] == 0) assert(img2(p) == 44);
+    else
+      assert(img2(p) == 43);
+  }
+
+  // One domains.
+  box2d domain = vpp::make_box2d(10, 10);
+  auto it = domain.begin();
+  vpp::pixel_wise(domain) << [&] (auto& p) {
+    assert(*it == p);
+    it.next();
+  };
+
+  vpp::col_wise(img2) << [&] (auto& p) { p = 45; };
+  for (auto p : img2.domain()) {
+    if (p[0] == 0) assert(img2(p) == 45);
+    else
+      assert(img2(p) != 45);
   }
 
 }
