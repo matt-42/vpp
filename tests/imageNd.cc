e4d124e53efe9901b5ad161d01f2c35df396d75e
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-05-14 16:01:17 +0200

    More tests for imageNd.

--- tests/imageNd.cc
+++ tests/imageNd.cc
@@ -5,9 +5,14 @@ int main()
 {
   using vpp::imageNd;
   using vpp::vint2;
+  using vpp::vint3;
+
 
   int dims[] = {100, 200};
+
+
   imageNd<int, 2> img(dims);
+
 
   assert(img.domain().size(0) == dims[0]);
   assert(img.domain().size(1) == dims[1]);
@@ -24,6 +29,7 @@ int main()
     for (int c = 0; c < img.domain().size(1); c++)
     {
       img(vint2(r, c)) = r * c;
+      img(r, c) = r * c;
     }
 
   for (int r = 0; r < img.domain().size(0) - 1; r++)
@@ -35,10 +41,17 @@ int main()
 
   // Test with border.
 
-  imageNd<int, 2> img2(dims, 1);
 
+  imageNd<int, 2> img2(dims, 1);
   assert(img2.pitch() == sizeof(int) * (dims[1] + 2));
   assert((char*)(&img2(vint2(0,0))) == ((char*)img2.data() + img2.pitch() + sizeof(int)));
+  assert((char*)(&img2(vint2(99,199))) == ((char*)&img2(0,0) + 99 * img2.pitch() + 199 * sizeof(int)));
+  assert((char*)img2.data_end() == ((char*)(img2.data()) + 102 * img2.pitch()));
+
+  int dim3[] = {100, 200, 300};
+  imageNd<int, 3> img3(dim3, 1);
+  assert(img3.pitch() == sizeof(int) * (dim3[2] + 2));
+  assert((char*)(&img3(vint3(0,0,0))) == ((char*)img3.data() + 1 * dim3[1] * img3.pitch() + img3.pitch() + sizeof(int) ));
 
   //assert(img.data() == )
 
