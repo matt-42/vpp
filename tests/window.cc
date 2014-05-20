77a3a84d63febc12c6c2e07ec51526d69cc72521
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-05-20 15:17:51 +0200

    Update window test to the new syntax.

--- tests/window.cc
+++ tests/window.cc
@@ -1,26 +1,35 @@
 #include <iostream>
-#include <vpp/image2d.hh>
-#include <vpp/window.hh>
+#include <vpp/vpp.hh>
 
 int main()
 {
-  using vpp::image2d;
-  using vpp::vint2;
-  using vpp::window;
+  using namespace vpp;
 
-  image2d<int> img1({3, 3});
+  image2d<int> A(100, 100);
 
-  window<image2d<int>> nbh(img1, {
+  auto win = make_window(A, {
       {-1, -1}, {-1, 0}, {-1, 1},
       {0, -1}, {0, 0}, {0, 1},
       {1, -1}, {1, 0}, {1, 1}
     });
 
+  int& pix = A(5, 5);
+
+  vint2 ref[] = {
+    vint2(4, 4),
+    vint2(4, 5),
+    vint2(4, 6),
+    vint2(5, 4),
+    vint2(5, 5),
+    vint2(5, 6),
+    vint2(6, 4),
+    vint2(6, 5),
+    vint2(6, 6)
+  };
+
   int i = 0;
-  for (auto& v : img1) v = i++;
-
-  i = 0;
-  for (auto& n : nbh(img1(vint2(1,1))))
-    assert(n == i++);
-
+  win(pix) < [&] (int& p) { assert( &p == &A(ref[i++])); };
+  assert(i == 9);
 }
+
+
