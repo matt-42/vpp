1a21ded021242d9f4bc946a1b307df255d5a9636
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2014-05-23 20:12:06 +0000

    Cleanup fast_detector benchmark.

--- benchmarks/fast_detector.cc
+++ benchmarks/fast_detector.cc
@@ -6,73 +6,66 @@
 #include <vpp/vpp.hh>
 #include <vpp/utils/opencv_bridge.hh>
 #include <vpp/algorithms/FAST_detector/FAST.hh>
-#include <vpp/algorithms/FAST_detector/FAST2.hh>
+// #include <vpp/algorithms/FAST_detector/FAST2.hh>
+// #include <vpp/algorithms/FAST_detector/FAST3.hh>
 
 #include "get_time.hh"
 
 int main(int argc, char* argv[])
 {
   using namespace vpp;
+  ::sched_param sc_params;
+  sc_params.sched_priority = 10;
+  if (::sched_setscheduler(::getpid(), SCHED_FIFO, &sc_params))
+    ::fprintf(stderr, "sched_setscheduler(): %s\n", ::strerror(errno));
 
-  if (argc != 3)
+  if (argc != 4)
   {
-    std::cerr << "Usage : " << argv[0] << " image threshold" << std::endl;
+    std::cerr << "Usage : " << argv[0] << " image threshold local_maxima_only" << std::endl;
     return 1;
   }
 
-  int K = 400;
+  int K = 40;
 
   typedef image2d<vuchar3> I;
   I A = clone_with_border(from_opencv<vuchar3>(cv::imread(argv[1])), 3);
   I B(A.domain(), 1);
 
+  image2d<unsigned char> big_image(10000,10000);
+
   image2d<unsigned char> Agl(A.domain(), 3);
   image2d<unsigned char> Bgl(A.domain());
-  image2d<int> tmp(A.domain());
+  image2d<int> tmp(A.domain(), 1);
 
   pixel_wise(Agl, A) << [] (unsigned char& gl, vuchar3& c)
   {
     gl = (c[0] + c[1] + c[2]) / 3;
   };
 
+  int lm = atoi(argv[3]);
   int th = atoi(argv[2]);
   auto time = get_time_in_seconds();
   std::vector<vint2> keypoints_vpp;
   for (unsigned i = 0; i < K; i++)
   {
     keypoints_vpp.clear();
-    fast_detector9(Agl, tmp, th);
-    keypoints_to_vector(tmp, keypoints_vpp);
+    fast_detector9(Agl, tmp, th, lm, false);
+    make_keypoint_vector(tmp, keypoints_vpp);
   }
-  //fast_detector<9>(Agl, Bgl, th);
   double vpp_ms_per_iter = 1000 * (get_time_in_seconds() - time) / K;
 
-  int vpp_n_keypoints = 0;
-  // for (int r = 0; r < Bgl.nrows(); r++)
-  // for (int c = 0; c < Bgl.ncols(); c++)
-  //   vpp_n_keypoints += Bgl(r, c);
-
-  pixel_wise(tmp) < [&] (auto& b)
-  {
-    vpp_n_keypoints += b;
-  };
-
-  // Opencv: void FAST(InputArray _img, std::vector<KeyPoint>& keypoints, int threshold, bool nonmax_suppression)
   time = get_time_in_seconds();
   cv::Mat cv_Agl = to_opencv(Agl);
   cv::Mat cv_B = to_opencv(B);
   std::vector<cv::KeyPoint> keypoints;
+  double vpp_time = 0, opencv_time = 0;
   for (unsigned i = 0; i < K; i++)
   {
     keypoints.clear();
-    FAST(cv_Agl, keypoints, th, false);
+    FAST(cv_Agl, keypoints, th, lm);
   }
-
-  std::cout << keypoints.size() << std::endl;
-
   double opencv_ms_per_iter = 1000 * (get_time_in_seconds() - time) / K;
 
-  std::cout << "vpp: " << vpp_n_keypoints << " keypoints" << std::endl;
   std::cout << "vpp: " << keypoints_vpp.size() << " keypoints" << std::endl;
   std::cout << "openmp: " << keypoints.size() << " keypoints" << std::endl;
 
@@ -80,32 +73,18 @@ int main(int argc, char* argv[])
             << "OpenCV: " << opencv_ms_per_iter << "ms" << std::endl
             << "VPP: " << vpp_ms_per_iter << "ms" << std::endl;
 
-  pixel_wise(Bgl) << [] (unsigned char& gl)
-  {
-    gl = gl * 255;
-  };
-
-  image2d<vuchar3> out(A.domain());
-  pixel_wise(out, Bgl, A) << [] (vuchar3& o, unsigned char& k, vuchar3& in)
+  image2d<vuchar3> vpp_out(A.domain());
+  image2d<vuchar3> opencv_out = clone(A);
+  pixel_wise(vpp_out, tmp, A) << [] (vuchar3& o, int& k, vuchar3& in)
   {
     if (!k)
-      o = in;
-    else
-      o = vuchar3(0,0, 255);
+      o = k ? in : vuchar3(0,0, 255);
   };
 
   for (int i = 0; i < keypoints.size(); i++)
-  {
-    //std::cout << "dupp"<< std::endl;
-    //Point pt = 
-    if (out(keypoints[i].pt.y, keypoints[i].pt.x) == vuchar3(0,0,255))
-      A(keypoints[i].pt.y, keypoints[i].pt.x) = vuchar3(0,255,0);
-    else
-      A(keypoints[i].pt.y, keypoints[i].pt.x) = vuchar3(0,0, 255);
-  }
+    opencv_out(keypoints[i].pt.y, keypoints[i].pt.x) = vuchar3(0, 0, 255);
 
-  cv::imwrite("b.pgm", to_opencv(Bgl));
-  cv::imwrite("out.ppm", to_opencv(out));
-  cv::imwrite("A.ppm", to_opencv(A));
+  cv::imwrite("opencv.ppm", to_opencv(opencv_out));
+  cv::imwrite("vpp.ppm", to_opencv(vpp_out));
 
 }
