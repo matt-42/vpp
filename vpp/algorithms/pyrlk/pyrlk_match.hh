99bb9edb70ce68c13760f4b2128a3098e246c017
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-06-05 21:12:27 +0200

    Fix pyrlk compilation.

--- vpp/algorithms/pyrlk/pyrlk_match.hh
+++ vpp/algorithms/pyrlk/pyrlk_match.hh
@@ -2,13 +2,19 @@
 # define VPP_ALGORITHMS_PYRLK_HH_
 
 # include <vpp/core/pyramid.hh>
+# include <vpp/core/keypoint_container.hh>
+# include <vpp/algorithms/pyrlk/lk.hh>
+# include <vpp/core/vector.hh>
 
 namespace vpp
 {
 
-  template <typename M, typename V, typename C>
-  void pyrlk_match(const pyramid2d<V>& pyramid_prev,
-                   const pyramid2d<V>& pyramid_next,
+  typedef keypoint_container<keypoint<float>, int> pyrlk_keypoint_container;
+
+  template <typename M, typename V, typename U, typename C>
+  void pyrlk_match(const pyramid2d<vector<V, 1>>& pyramid_prev,
+                   const pyramid2d<vector<U, 2>>& pyramid_prev_grad,
+                   const pyramid2d<vector<V, 1>>& pyramid_next,
                    C& keypoints,
                    M matcher,
                    float min_ev, float max_err)
@@ -21,7 +27,7 @@ namespace vpp
       for(int S = 0; S < pyramid_prev.size(); S++)
       {
         tr *= pyramid_prev.factor();
-        auto match = M::match(kp.position, tr, pyramid_prev[S], pyramid_next[S], pyramid_prev_grad[S]);
+        auto match = matcher(kp.position, tr, pyramid_prev[S], pyramid_next[S], pyramid_prev_grad[S], min_ev);
         if (match.second < max_err)
           tr = match.first;
         else
@@ -33,7 +39,7 @@ namespace vpp
 
       kp.position += tr;
       kp.age++;
-      keypoints.update_index(i, kp.position.cast<int>());
+      // fixme keypoints.update_index(i, kp.position.cast<int>());
 
     nextpoint:
       continue;
