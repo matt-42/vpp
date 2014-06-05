30fa85c068d642ec6b5e67d17456d833fa30c4c9
Matthieu Garrigues <matthieu.garrigues@ensta.fr>
2014-06-05 21:05:38 +0200

    Refactor and continue pyrlk example.

--- examples/pyrlk.cc
+++ examples/pyrlk.cc
@@ -4,17 +4,13 @@
 #include <vpp/vpp.hh>
 #include <vpp/utils/opencv_bridge.hh>
 #include <vpp/algorithms/FAST_detector/FAST.hh>
-//#include <vpp/algorithms/pyrlk/pyrlk_match.hh>
+#include <vpp/algorithms/pyrlk/pyrlk_match.hh>
+#include <vpp/algorithms/filters/scharr.hh>
 
 using namespace vpp;
 
-
-template <typename F, typename G>
-void for_each_video_frames(const char* str, F fun_init, G fun_loop)
+bool open_videocapture(const char* str, cv::VideoCapture& cap)
 {
-
-  cv::VideoCapture cap;
-
   long int device = atoi(str);
   if (device > 0)
     cap.open(device);
@@ -23,21 +19,16 @@ void for_each_video_frames(const char* str, F fun_init, G fun_loop)
   if (!cap.isOpened())
   {
     std::cerr << "Error: Cannot open " << str << std::endl;
-    return;
+    return false;
   }
 
-  image2d<vuchar3> frame(cap.get(CV_CAP_PROP_FRAME_HEIGHT),
-                         cap.get(CV_CAP_PROP_FRAME_WIDTH));
+  return true;
+}
 
-  fun_init(frame.domain());
-
-  std::cout << "Video size: " << frame.nrows() << " " << frame.ncols() << std::endl;
-
-  cv::Mat cvframe = to_opencv(frame);
-
-  int frame_cpt = 0;
-  while (cap.read(cvframe))
-    fun_loop(frame);
+box2d videocapture_domain(cv::VideoCapture& cap)
+{
+  return make_box2d(cap.get(CV_CAP_PROP_FRAME_HEIGHT),
+                    cap.get(CV_CAP_PROP_FRAME_WIDTH));
 }
 
 int main(int argc, char* argv[])
@@ -50,26 +41,39 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  cv::VideoCapture cap;
+  if (!open_videocapture(argv[1], cap)) return 1;
+
+  int nscales = atoi(argv[2]);
+  int fast_th = atoi(argv[3]);
+
   int frame_cpt = 0;
 
-  for_each_video_frames(
+  box2d domain = videocapture_domain(cap);
+  image2d<vuchar3> frame(domain);
+  image2d<vuchar1> framegl(domain);
+  cv::Mat cvframe = to_opencv(frame);
 
-    argv[1],
+  pyramid2d<vuchar1> pyramid1(frame.domain(), nscales, 2);
+  pyramid2d<vuchar1> pyramid2(frame.domain(), nscales, 2);
 
-    // Init
-    [&] (const box2d& domain)
-    {
-      std::cout << domain.size(0) << "x" << domain.size(1) << std::endl;
-    },
+  pyramid2d<vfloat2> pyramid1_grad(frame.domain(), nscales, 2);
 
-    // Loop
-    [&] (const image2d<vuchar3>& frame)
-    {
-      std::cout << frame_cpt << std::endl;
-      frame_cpt++;
-    }
+  pyrlk_keypoint_container keypoints(domain);
+  while (cap.read(cvframe))
+  {
+    pixel_wise(frame, pyramid1[0]) << [] (const vuchar3& in, vuchar1& out) { out[0] = (in[0] + in[1] + in[2]) / 3; };
+    pyramid1.propagate_level0();
 
-    );
+    scharr(pyramid1[0], pyramid1_grad[0]);
+    pyramid1_grad.propagate_level0();
+
+    pyrlk_match(pyramid1, pyramid1_grad, pyramid2, keypoints, lk_match_point_square_win<5>(), 0.01, 3);
+
+    std::cout << frame_cpt << std::endl;
+    frame_cpt++;
+
+    pyramid1.swap(pyramid2);
+  }
 
 }
-
