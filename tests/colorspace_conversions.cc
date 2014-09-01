0063b860fc6abf623dddab54361d9dc40a7eee16
Matthieu Garrigues <matthieu.garrigues@ensta-paristech.fr>
2014-09-01 20:35:40 +0200

    Test colorspace conversions.

---
+++ tests/colorspace_conversions.cc
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <vpp/vpp.hh>
+
+int main()
+{
+  using namespace vpp;
+
+  image2d<vuchar3> i1(100, 100);
+
+  unsigned char i = 0;
+  for (vint2 p : i1.domain())
+  {
+    i1(p) = vuchar3{i, i, i};
+    i++;
+  }
+
+  image2d<vuchar1> i2 = rgb_to_graylevel<unsigned char>(i1);
+  i = 0;
+  for (vint2 p : i1.domain())
+  {
+    assert(i2(p)[0] == i);
+    i++;
+  }
+
+}
