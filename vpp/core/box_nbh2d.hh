f1c6e7c2a314414d1cc97245d8d6a4a6a94b4d9b
Matthieu Garrigues <matthieu.garrigues@ensta-paristech.fr>
2014-09-05 21:46:01 +0200

    Optimize nbh2d for 2d convolutions

--- vpp/core/box_nbh2d.hh
+++ vpp/core/box_nbh2d.hh
@@ -20,7 +20,7 @@ namespace vpp
       }
 
     template <typename F>
-    inline void operator<(F f)
+    inline void for_all(F f)
       {
         for (int r = -nrows/2; r <= nrows/2; r++)
           for (int c = -ncols/2; c <= ncols/2; c++)
@@ -32,12 +32,79 @@ namespace vpp
   };
 
   template <typename V, template <class> class Const, int nrows, int ncols>
+  struct box_nbh2d_accessor
+  {
+    box_nbh2d_accessor()
+    {
+    }
+
+    template <int r, int c>
+    Const<V>& at()
+    {
+      return rows_[r + nrows / 2][c];
+    }
+
+    Const<V>& at(int r, int c)
+    {
+      return rows_[r + nrows / 2][c];
+    }
+
+
+    template <int r>
+    Const<V>* row()
+    {
+      return rows_[r + nrows / 2];
+    }
+
+    template <typename F>
+    void for_all(F f)
+    {
+      for (int r = -nrows/2; r <= nrows/2; r++)
+        for (int c = -ncols/2; c <= ncols/2; c++)
+          f(at(r, c));
+    }
+
+    Const<V>* rows_[nrows];
+  };
+
+  template <typename V, template <class> class Const, int nrows, int ncols>
+  struct box_nbh2d_;
+
+  template <typename V, template <class> class Const, int nrows, int ncols>
+  struct box_nbh2d_row_iterator
+  {
+    typedef box_nbh2d_accessor<V, Const, nrows, ncols> accessor_type;
+
+    box_nbh2d_row_iterator(vint2 p, const box_nbh2d_<V, Const, nrows, ncols>& box)
+    {
+      for (int r = -nrows / 2; r <= nrows / 2; r++)
+        accessor_.rows_[r + nrows / 2] = (Const<V>*)((char*)box.begin_ + box.pitch_ * (p[0] + r));
+    }
+
+    void next()
+    {
+      for (int i = 0; i < nrows; i++)
+        accessor_.rows_[i]++;
+    }
+    
+    accessor_type& operator*()
+    {
+      return accessor_;
+    }
+
+    accessor_type accessor_;
+  };
+
+  template <typename V, template <class> class Const, int nrows, int ncols>
   struct box_nbh2d_
   {
+    typedef box_nbh2d_row_iterator<V, Const, nrows, ncols> row_iterator;
+
     inline box_nbh2d_(Const<image2d<V>>& img)
-      : pitch_(img.pitch())
-      {
-      }
+      : pitch_(img.pitch()),
+        begin_(&img(0,0))
+    {
+    }
 
     inline box_nbh2_runner<V, Const, nrows, ncols> operator()(Const<V>& pix)
     {
@@ -45,6 +112,7 @@ namespace vpp
     }
 
     int pitch_;
+    Const<V>* begin_;
   };
 
   template <typename V, int nrows, int ncols>
