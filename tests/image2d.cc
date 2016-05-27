5e57fa486eaeda91d77fb7e45ad1ea1a22096dca
Matthieu Garrigues <matthieu.garrigues@ensta-paristech.fr>
2016-05-27 12:44:32 +0200

    Test operator[][]

--- tests/image2d.cc
+++ tests/image2d.cc
@@ -3,11 +3,9 @@
 
 int main()
 {
-  using vpp::image2d;
-  using vpp::vint2;
-  using vpp::make_box2d;
+  using namespace vpp;
 
-  image2d<int> img1(make_box2d(100, 200));
+  image2d<int> img1(make_box2d(100, 200), _border = 3);
   image2d<int> img2({100, 200});
 
   assert(img1.domain() == img2.domain());
@@ -15,4 +13,13 @@ int main()
   assert(img1.nrows() == 100);
   assert(img1.ncols() == 200);
 
+  {
+    image2d<int> img(make_box2d(5, 5), _border = 3);
+    
+    auto s1 = img.subimage(img.domain());
+    for (auto p : img.domain())
+      assert(img(p) == img[p[0]][p[1]]);
+    for (auto p : img.domain())
+      assert(img(p) == s1[p[0]][p[1]]);
+  }
 }
