cef7b3b9de66602f6f2533e0a522db09329de245
Matthieu Garrigues <matthieu.garrigues@ensta-paristech.fr>
2016-12-14 17:05:37 +0100

    Rgb colors.

---
+++ vpp/draw/rgb_colors.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <eigen/core>
+
+namespace vpp
+{
+  namespace rgb_colors
+  {
+    vuchar3 blue(0,0,255);
+    vuchar3 green(0,255,0);
+    vuchar3 red(255,0,0);
+
+  }
+  
+}
