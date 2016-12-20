531ce7913852511760cc72032b52363ccd4d3e57
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2016-12-20 12:06:03 +0100

    Fix compilation on osx.

--- vpp/core/keypoint_container.hh
+++ vpp/core/keypoint_container.hh
@@ -13,7 +13,7 @@ namespace vpp
   struct keypoint
   {
     keypoint() : age(0) {}
-    keypoint(vector<C, 2> pos) : position(pos), velocity(zero<vector<C, 2>>()),
+    keypoint(vector<C, 2> pos) : position(pos), velocity(0,0),
                                  age(1) {}
 
     vector<C, 2> position;
