ca4c3b901a7661612f457c45021f13b78b6a9c3a
Yannick Zoetgnande <yannickzoet@live.fr>
2017-09-22 11:04:24 +0200

    paint points
    Paint points instead of painting lines

--- vpp/algorithms/Line_tracker_4_sfm/Hough_Extruder/paint.hh
+++ vpp/algorithms/Line_tracker_4_sfm/Hough_Extruder/paint.hh
@@ -128,6 +128,79 @@ void paint_original_video(std::vector<track>& trs,
         }
     }
 }
+  
+  
+  void paint_original_video_particle(std::vector<track>& trs,
+                          image2d<vuchar4>& paint_buffer,int T_theta,
+                          int nrows,int ncols)
+{
+
+    // Decrease opacity.
+    pixel_wise(paint_buffer) | [] (vuchar4& v) {
+        v[3] *= 0.97;
+    };
+
+//#pragma omp parallel for
+    for (int i = 0; i < trs.size(); i++)
+    {
+        auto& t = trs[i];
+        if (!t.isAlive() or t.size() == 0) continue;
+
+        //if (t.alive() && t.size() > 1)
+        {
+
+            vint2 p1 = t.position_at_frame(t.last_frame()).template cast<int>();
+            vint2 p2 = t.position_at_frame(t.last_frame() - 1).template cast<int>();
+            vint2 p3 = t.position_at_frame(t.last_frame() - std::min(t.size() - 1, 10)).template cast<int>();
+
+            float speed = (p3 - p1).norm();
+            vuchar3 pt_color = hsv_to_rgb((M_PI + atan2(p3[0] - p1[0], p3[1] - p1[1])) * 180.f / M_PI,
+                    1.f, 1.f);
+
+            float alpha = std::min(1.f, speed / 10);
+
+            auto paint = [&] (vint2 x, float a)
+            {
+                if (!paint_buffer.has(x)) return;
+                vuchar4 c;
+                //c.template segment<3>(0) = pt_color;
+                c.template segment<3>(0) = t.color;
+                c[3] = 255*1*alpha;
+                vpp::draw::plot_color(paint_buffer, x, c);
+            };
+
+            auto paint2 = [&] (vint2 x, float a, int d)
+            {
+                if (!paint_buffer.has(x)) return;
+                vuchar4 c;
+                c.template segment<3>(0) = t.color;
+                c[3] = 255*0.7*alpha;
+                vpp::draw::plot_color(paint_buffer, x, c);
+            };
+
+
+
+            if ((p1 - p3).norm() > 5)
+            {
+                std::list<vint2> list_p1 = t.list_point_at_frame_image(t.last_frame());
+                std::list<vint2> list_p2 = t.list_point_at_frame_image(t.last_frame() - 1);
+                //cout << "here" << endl;
+                float cosinus,sinus;
+                vint4 ligne = getLineFromPoint_Fast(p1[0],p1[1],T_theta,nrows,ncols,cosinus,sinus);
+                line2d_hough_particles(list_p1,vint2(ligne[1],ligne[0]), vint2(ligne[3],ligne[2]), paint, paint2);
+                ligne = getLineFromPoint_Fast(p2[0],p2[1],T_theta,nrows,ncols,cosinus,sinus);
+                line2d_hough_particles(list_p2,vint2(ligne[1],ligne[0]), vint2(ligne[3],ligne[2]), paint, paint2);
+                /*float cosinus,sinus;
+                image2d<uchar> grad_img = t.gradient_image_at_frame(t.last_frame());
+                vint4 ligne = getLineFromPoint_Fast(p1[0],p1[1],T_theta,nrows,ncols,cosinus,sinus);
+                line2d_hough(vint2(ligne[1],ligne[0]), vint2(ligne[3],ligne[2]), paint, paint2,grad_img);
+                ligne = getLineFromPoint_Fast(p2[0],p2[1],T_theta,nrows,ncols,cosinus,sinus);
+                grad_img = t.gradient_image_at_frame(t.last_frame() - 1);
+                line2d_hough(vint2(ligne[1],ligne[0]), vint2(ligne[3],ligne[2]), paint, paint2,grad_img);*/
+            }
+        }
+    }
+}
 
 
 }
