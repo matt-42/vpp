f811e049534b69b1f2beef39d65dd4d24e78cab8
Yannick Zoetgnande <yannickzoet@live.fr>
2017-09-27 16:40:28 +0200

    Update hough_extruder.cc

--- examples/hough_extruder.cc
+++ examples/hough_extruder.cc
@@ -14,29 +14,24 @@ using namespace std::chrono;
 int main(int argc, char *argv[])
 {
     high_resolution_clock::time_point t1 = high_resolution_clock::now();
-    //image3d<vuchar3> img3;
-    //feature_matching_video
-    //mode_capture_photo
-    Capture_Image(feature_matching_video,Theta_max::SMALL, Sclare_rho::HALF,
+    Fast_DHT_Matching(feature_matching,Theta_max::SMALL, Sclare_rho::SAME,
                   Type_video_hough::ALL_POINTS,
                   Type_output::ORIGINAL_VIDEO,
                   Type_Lines::ONLY_POLAR,
                   Frequence::ALL_FRAME,
                   With_Kalman_Filter::NO,
                   With_Transparency::YES,
+                  With_Entries::YES,
                   _rayon_exclusion_theta = 15,
                   _rayon_exclusion_rho = 12,
                   _slot_hough = 1,
-                  _link_of_video = "videos/work1.avi",
-                  _detector_th = 10,
-                  _keypoint_spacing = 10,
-                  _detector_period = 1,
+                  _link_of_video_image = "videos/corridor2.mp4",
                   _acc_threshold = 100,
                   _m_first_lines = 5,
                   _max_trajectory_length = 5,
                   _nombre_max_frame_without_update =5);/**/
     high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    auto duration = duration_cast<seconds>( t2 - t1 ).count();
+    auto duration = duration_cast<nanoseconds>( t2 - t1 ).count();
     cout << "la duree " << duration << endl ;
     return 0;
 }
