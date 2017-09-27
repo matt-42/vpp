5a4df3994314b511902a329b28a08f6a488dcfc2
Yannick Zoetgnande <yannickzoet@live.fr>
2017-09-27 16:41:23 +0200

    Update dense_one_to_one_hough.hh

--- vpp/algorithms/Line_tracker_4_sfm/Fast_DHT/dense_one_to_one_hough.hh
+++ vpp/algorithms/Line_tracker_4_sfm/Fast_DHT/dense_one_to_one_hough.hh
@@ -7,7 +7,7 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
 #include <vpp/vpp.hh>
-#include <Eigen/Core>
+#include <eigen3/Eigen/Core>
 #include <vpp/utils/opencv_bridge.hh>
 #include <iod/timer.hh>
 
