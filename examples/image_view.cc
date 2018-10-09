2e17b21c56680bcfa94292ef5117f73572bf277d
wangl <603020460@qq.com>
2018-10-09 19:37:16 +0800

    add hehe

--- examples/image_view.cc
+++ examples/image_view.cc
@@ -2,7 +2,7 @@
 int main()
 {
   image2d<vuchar3> rgb(100,100);
-
+  //hehe
   auto red = image_view(rgb, [] (vint2 p) { return rgb(p)[0]; });
   auto sub_red = image_view(rgb.subimage(vint2(0,0), vint2(10,10)),
                             [] (vint2 p) { return rgb(p)[0]; });
